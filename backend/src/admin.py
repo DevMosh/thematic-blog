0ab8a1e7666ca73d4db348f7bd4146f819b2199c
DevMosh <49912219+DevMosh@users.noreply.github.com>
2024-07-02 17:22:57 +0300

    models added to admin panel

--- backend/src/admin.py
+++ backend/src/admin.py
@@ -1,9 +1,18 @@
 
-# from django.contrib import admin
-#
-# from src.models.comment import Comment
-#
-#
+from django.contrib import admin
+
+from src.models import Article, Comment, CommentLike, Category, ArticleFavorites
+
+admin.site.register(
+    [
+        Category,
+        Article,
+        ArticleFavorites,
+        Comment,
+        CommentLike,
+    ]
+)
+
 # @admin.register(Comment)
 # class CommentAdmin(admin.ModelAdmin):
 #     list_display = ('')
